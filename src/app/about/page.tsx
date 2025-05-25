a1925f8ad982ee146ea95a98abed12711267b2d6
Sitraka <hobynyaina.sitraka@email.com>
2025-05-25 22:46:16 +0300

    feat: 'Arrangement de la page About'

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import Image from 'next/image';
-import profilImage from '@/assets/images/profil.jpg';
-import Skill from '@/app/skills/page';
+import profilImage from '@/assets/images/profil-sitraka.jpg';
 import { motion } from 'framer-motion';
+import CoverLetter from '@/app/cover-letter/page';
+import Competence from '@/app/competence/page';
 
 type Stat = {
   value: string;
@@ -19,15 +20,6 @@ const stats: Stat[] = [
   { value: '65+', label: 'INTERNATIONAL AWARDS WON' },
 ];
 
-const skills = [
-  { label: 'HTML5 / CSS3', percent: 90 },
-  { label: 'JavaScript', percent: 85 },
-  { label: 'React / Next.js', percent: 45 },
-  { label: 'Node.js', percent: 75 },
-  { label: 'Tailwind CSS', percent: 90 },
-  { label: 'UI/UX Design', percent: 80 },
-];
-
 export default function About() {
   return (
     <>
@@ -39,9 +31,9 @@ export default function About() {
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 0.05, y: 0 }}
           transition={{ duration: 0.8 }}
-          className="absolute top-[70] inset-0 text-9xl md:text-9xl font-bold text-white select-none pointer-events-none z-0"
+          className="absolute top-[80] inset-0 text-8xl md:text-9xl font-bold text-white select-none pointer-events-none z-0"
         >
-          Resume
+          About
         </motion.span>
 
         {/* Titre principal */}
@@ -51,14 +43,14 @@ export default function About() {
           transition={{ duration: 0.6 }}
           className="relative text-6xl font-bold mb-20 z-10"
         >
-          ABOUT <span className="text-sky-500">ME</span>
+          A <span className="text-[#00BDC8]">PROPOS</span>
         </motion.h2>
 
         <div className="grid md:grid-cols-2 gap-10 items-center z-10 relative">
           {/* Infos personnelles */}
           <div className="flex flex-col md:flex-row items-center gap-6">
             <div className="relative w-48 h-60">
-              <div className="absolute inset-0 border-[3px] border-sky-500 -translate-x-2 -translate-y-2" />
+              <div className="absolute inset-0 border-[3px] border-[#00BDC8] -translate-x-2 -translate-y-2" />
               <Image
                 src={profilImage}
                 alt="Profile"
@@ -71,13 +63,11 @@ export default function About() {
 
             <div className="text-sm md:text-base space-y-1 text-left">
               <p><strong>Name:</strong> Sitraka Ny Aina</p>
-              <p><strong>Age:</strong> 26</p>
               <p><strong>Phone:</strong> +261 38 99 262 17</p>
-              <p><strong>Skype:</strong> Briscoe_fc</p>
-              <p><strong>Email:</strong> FrancisMBriscoe@jourrapide.com</p>
-              <p><strong>Address:</strong> 4080 Berkshire Circle</p>
-              <p><strong>Languages:</strong> French, English</p>
-              <p><strong>Nationality:</strong> USA</p>
+              <p><strong>Email:</strong> hobynyaina.sitraka@gmail.com</p>
+              <p><strong>Address:</strong> Lot IC 177 ter A Soamahavory</p>
+              <p><strong>Languages:</strong> French</p>
+              <p><strong>Nationality:</strong> Malgache</p>
             </div>
           </div>
 
@@ -100,26 +90,8 @@ export default function About() {
         </div>
       </div>
     </section>
-
-      {/* MY SKILL */}
-      <section className="bg-[#0f172a] text-white py-5 px-4 md:px-16">
-        <div className="max-w-7xl mx-auto text-center">
-          <motion.h2
-            initial={{ opacity: 0, y: -20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6 }}
-            className="text-6xl font-bold mb-15"
-          >
-            <span className="text-sky-500">MY</span> SKILL
-          </motion.h2>
-          
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 justify-items-center">
-            {skills.map((skill, index) => (
-              <Skill key={index} {...skill} />
-            ))}
-          </div>
-        </div>
-      </section>
+    <Competence/>
+    <CoverLetter />
     </>
   );
 }
