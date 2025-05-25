d61a31d1525a2ca0d0461c754c7459dd256307d2
Sitraka <hobynyaina.sitraka@email.com>
2025-05-25 22:43:45 +0300

    feat: 'Changement du données'

---
+++ src/data/educations/page.ts
@@ -0,0 +1,11 @@
+const educations = [
+    {
+      title: 'Licence en Communication Audiovisuelle et Numérique',
+      years: '2021 - 2024',
+      institution: 'E-Média Madagascar',
+      description: 'Options : Développement Web',
+    },
+  ]
+  
+  export default educations
+  
---
+++ src/data/workingExperiences/page.ts
@@ -0,0 +1,18 @@
+const workingExperiences = [
+    {
+      title: 'Développeur Php',
+      years: 'Mars 2025 - Mai 2025',
+      company: 'Atout Service Mada',
+      description: 'Stage | 3 mois',
+      role: 'Développer un site',
+    },
+    {
+      title: 'Développeur Javascript',
+      years: 'Fevrier 2025',
+      company: '',
+      description: 'Test pour un stage',
+    },
+  ]
+  
+  export default workingExperiences
+  
