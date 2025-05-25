b7942a8824e4f885f3ee7bc78216bfd0289fcf60
Sitraka <hobynyaina.sitraka@email.com>
2025-05-25 22:38:23 +0300

    feat: 'Qualité du profil'

--- src/components/sidebar/page.tsx
+++ src/components/sidebar/page.tsx
@@ -4,7 +4,7 @@ import { NAV_ITEMS } from '@/config/navigation'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 import Link from 'next/link'
-import profilImage from '@/assets/images/profil.jpg'
+import profilImage from '@/assets/images/profil-sitraka.jpg'
 import MobileNavbar from '@/components/sidebar/app-sidebar-mobile'
 import { useState } from 'react'
 
@@ -13,38 +13,33 @@ export default function Sidebar() {
 
   return (
     <>
-      {/* Version Mobile */}
       <MobileNavbar activeItem={activeItem} setActiveItem={setActiveItem} />
-
-      {/* Version Desktop */}
       <div className="hidden md:flex fixed inset-y-0 left-0 w-25 bg-gray-900 text-white flex-col items-center justify-center z-50 border-r border-gray-700">
         <div className="flex flex-col items-center justify-center h-full space-y-5">
-          {/* Photo de profil */}
-          <div className="w-20 h-20 rounded-full overflow-hidden border-2 border-white/20 hover:border-sky-500 transition-all duration-300">
-            <Image
-              src={profilImage}
-              alt="Profil"
-              width={64}
-              height={64}
-              className="object-cover w-full h-full hover:scale-105 transition-transform duration-300"
-            />
-          </div>
-          
-          {/* Icônes */}
+        <div className="w-24 h-24 rounded-full overflow-hidden border-4 border-white/10 hover:border-[#00BDC8] shadow-md hover:shadow-[#00BDC8] transition-all duration-300">
+              <Image
+                src={profilImage}
+                alt="Photo de profil"
+                width={128}
+                height={128}
+                quality={100}
+                priority
+                className="object-cover w-full h-full rounded-full hover:scale-105 transition-transform duration-500"
+              />
+            </div>
           <nav className="flex flex-col items-center space-y-3">
-            {NAV_ITEMS.map((item) => (
+            {NAV_ITEMS.filter(item => !item.hidden).map((item) => (
               <Link 
                 href={item.path} 
                 key={item.path}
                 onClick={() => setActiveItem(item.path)}
                 className="relative group"
-              >
-                <motion.div
+              ><motion.div
                   whileHover={{ scale: 1.1 }}
-                  className={`p-3 rounded-full transition-colors bg-gray-700 text-black text-2xl ${
+                  className={`p-3 rounded-full transition-colors bg-gray-700 text-2xl ${
                     activeItem === item.path
-                      ? 'bg-sky-500'
-                      : 'text-gray-400 hover:bg-sky-500 hover:text-black'
+                    ? 'bg-sky-500/20 text-[#00BDC8]'
+                    : 'text-gray-400 hover:bg-[#00BDC8] hover:text-black'
                   }`}
                 >
                   {<item.icon/>}
